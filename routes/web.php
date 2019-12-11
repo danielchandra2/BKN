dba6244c72f6bd6c7f954bdd13ff47c8b9ab5ff2
danielchandra2 <56572103+danielchandra2@users.noreply.github.com>
2019-12-11 15:31:58 +0700

    admin controller

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', function () {
-    return view('/admin/admin');
-});
+Route::get('/admin', 'AdminController@index');
